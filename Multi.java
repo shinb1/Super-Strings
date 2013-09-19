1de4ef652d013087a34ac0cb3c2c5600641855e1
apcs <apcs@mvhs-imac31u528.dcsdk12.org>
2013-09-19 08:13:51 -0600

    Finished homework

--- Multi.java
+++ Multi.java
@@ -5,11 +5,12 @@ public class Multi {
 				    {6, 7, 8, 9, 10},
 				    {11, 12, 13, 14, 15}  };
 
-		sumCol(a);    
+		snakeDraft(10,3);
+		print(a);   
 
 	}
 
-	public void print(int[][] a) {
+	public static void print(int[][] a) {
 		for (int r=0; r < a.length; r++) {
 			for (int c=0; c < a[r].length; c++) {
 				
@@ -24,55 +25,122 @@ public class Multi {
 
 	}
 
-	public static int sumRow(int[][] a) {
+	// public static int sumRow(int[][] a) {
 		
-		int sum = 0;
-		int f = 1;
+	// 	int sum = 0;
+	// 	int f = 1;
 
-		for (int r=0; r <a.length; r++) {
-			sum = 0;
+	// 	for (int r=0; r <a.length; r++) {
+	// 		sum = 0;
 
-			for (int c=0; c < a[r].length; c++) {
+	// 		for (int c=0; c < a[r].length; c++) {
 
 
-				sum = sum + a[r][c];
+	// 			sum = sum + a[r][c];
+	// 		}
+
+
+	// 		System.out.println("row " + f + " = " + sum);
+	// 		f++;
+
+
+	// 	}
+
+	// 	return sum;
+	// }
+
+	// public static int sumCol(int[][] a) {
+		
+	// 	int sum = 0;
+	// 	int f = 1;
+	// 	int r=0;
+
+	// 	for (int c=0; c <a[r].length; c++) {
+	// 		sum = 0;
+
+	// 		for (r=0; r < a.length; r++) {
+
+
+	// 			sum = sum + a[r][c];
+	// 		}
+
+
+	// 		System.out.println("Column " + f + " = " + sum);
+	// 		f++;
+
+	// 		r=0;
+	// 	}
+
+	// 	return sum;
+	// }
+
+	// public static int sumTotal(int[][] a) {
+
+	// 	int sum = 0;
+	// 	int total = 0;
+
+	// 	for (int r=0; r <a.length; r++) {
+	// 		sum = 0;
+
+	// 		for (int c=0; c < a[r].length; c++) {
+
+
+	// 			sum = sum + a[r][c];
+	// 		}
+
+
+	// 		total = total + sum;
+	// 	}
+
+		
+	// 	System.out.println("The total value is: "+total);
+
+	// 	return total;
+	// }
+
+	// public static int[][] reverseRow(int[][] a) {
+
+	// 	int sum = 0;
+	// 	int f = 1;
+
+	// 	for (int r=0; r <a.length; r++) {
+	// 		sum = 0;
+
+	// 		for (int c=0; c < a[r].length; c++) {
+
+
+	// 			sum = sum + a[r][c];
+	// 		}
+
+
+	// 		System.out.println("row " + f + " = " + sum);
+	// 		f++;
+
+
+	// 	}
+
+	// 	return a;
+
+	// }
+
+
+	public static int[][] snakeDraft(int players, int rounds) {
+
+		int[][] a = new int[players][rounds];
+		
+		for (int r=0; r<rounds; r++) {
+			for (int c=0; c<players; c++) {
+				int count=1 + (r*10);
+				a[r][c] = count;
+				count ++;
 			}
-
-
-			System.out.println("row " + f + " = " + sum);
-			f++;
 
 
 		}
 
-		return sum;
+		return a;
+
 	}
-
-	public static int sumCol(int[][] a) {
-		
-		int sum = 0;
-		int f = 1;
-		int r=0;
-
-		for (int c=0; c <a[r].length; c++) {
-			sum = 0;
-
-			for (r=0; r < a.length; r++) {
-
-
-				sum = sum + a[r][c];
-			}
-
-
-			System.out.println("Column " + f + " = " + sum);
-			f++;
-
-			r=0;
-		}
-
-		return sum;
-	}
-
 
 
 
