e34d496515e4bd859e865ad3ef87b21b0e5e968c
apcs <apcs@mvhs-imac31u528.dcsdk12.org>
2013-09-19 08:30:32 -0600

    Finished Homework for real

--- Multi.java
+++ Multi.java
@@ -5,8 +5,10 @@ public class Multi {
 				    {6, 7, 8, 9, 10},
 				    {11, 12, 13, 14, 15}  };
 
-		snakeDraft(10,3);
-		print(a);   
+		// snakeDraft(10,3);
+		// print(a);   
+		int[] b = {1, 2, 3, 4, 5, 6};
+		print(rectangularize(b, 3, 2));
 
 	}
 
@@ -124,23 +126,41 @@ public class Multi {
 	// }
 
 
-	public static int[][] snakeDraft(int players, int rounds) {
+	// public static int[][] snakeDraft(int players, int rounds) {
 
-		int[][] a = new int[players][rounds];
+	// 	int[][] a = new int[players][rounds];
 		
-		for (int r=0; r<rounds; r++) {
-			for (int c=0; c<players; c++) {
-				int count=1 + (r*10);
-				a[r][c] = count;
-				count ++;
+	// 	for (int r=0; r<rounds; r++) {
+	// 		for (int c=0; c<players; c++) {
+	// 			int count=1 + (r*10);
+	// 			a[r][c] = count;
+	// 			count ++;
+	// 		}
+
+
+	// 	}
+
+	// 	return a;
+
+	//}
+
+
+	public static int[][] rectangularize(int[] a, int rows, int cols) {
+		int[][] result = new int[rows][cols];
+
+		int counter = 0;
+
+		for (int i = 0; i < a.length; i++) {
+			for (int j=0; j<result[i].length; j++) {
+				result[i][j] = a[counter];
+				counter++;
 			}
 
 
 		}
-
-		return a;
-
+		return result; 
 	}
+
 
 
 
